6bff8a19b7b891a549a2d4cd2c14c1ff8b0d2c60
Hasitha Chamod <32579928+hasithachamod@users.noreply.github.com>
2017-10-20 22:49:00 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String variable1 = "Hello World"; //added a string and defined it to "Hello World" text//
+        System.out.println(variable1); //printed the defined string//
     }
 }
